59c0575cff048a1e28e4e5d321ff1bf0f2b98f28
Nicholas Bowers <greennick7777@gmail.com>
2023-08-21 23:53:59 -0400

    Update app launcher

--- .config/eww/scripts/search.sh
+++ .config/eww/scripts/search.sh
@@ -1,47 +1,71 @@
-#!/bin/sh
+#!/bin/bash
 IFS=':' read -ra DIRS <<< $XDG_DATA_DIRS
 DIRS+=($XDG_DATA_HOME)
 FILES=()
-for DIR in "${DIRS[@]}"; do
-  if [[ $DIR != */ ]]; then
-    DIR="${DIR}/applications"
-  else
-    DIR="${DIR}applications"
-  fi
-
-  if [[ -d $DIR ]]; then
-    FILES+=($(find $DIR/*.desktop))
-  fi
-done
-
-for FILE in "${FILES[@]}"; do
-  PARSE=false
-  HIDDEN=false
-  NO_DISPLAY=false
-  while IFS='' read -r LINE; do
-    if [[ $LINE == "[Desktop Entry]" ]]; then
-      PARSE=true
-      continue
+for DIR in "${DIRS[@]}";
+do
+    if [[ $DIR != */ ]];
+    then
+        DIR="${DIR}/applications"
+    else
+        DIR="${DIR}applications"
     fi
 
-    if [[ $PARSE == "true" ]]; then
-      case $LINE in
-        \[*\])
-          break;;
-        Name=*)
-          NAME=${LINE#*=};;
-        Icon=*)
-          ICON=${LINE#*=};;
-        Hidden=*)
-          HIDDEN=${LINE#*=};;
-        NoDisplay=*)
-          NO_DISPLAY=${LINE#*=};;
-      esac
+    if [[ -d $DIR ]]; then
+        FILES+=($(find $DIR/*.desktop))
     fi
-  done < $FILE
-
-  if [[ $HIDDEN == "true" || $NO_DISPLAY == "true" ]]; then
-    continue
-  fi
-  echo $NAME
 done
+
+TEST=0
+for FILE in "${FILES[@]}";
+do
+    PARSE=false
+    HIDDEN=false
+    NO_DISPLAY=false
+    while IFS='' read -r LINE;
+    do
+        if [[ $LINE == "[Desktop Entry]" ]];
+        then
+            PARSE=true
+            continue
+        fi
+
+        if [[ $PARSE == "true" ]];
+        then
+            case $LINE in
+                \[*\])
+                    break;;
+                Name=*)
+                    NAME=${LINE#*=};;
+                Icon=*)
+                    ICON=${LINE#*=};;
+                Exec=*)
+                    EXEC=${LINE#*=};;
+                Hidden=*)
+                    HIDDEN=${LINE#*=};;
+                NoDisplay=*)
+                    NO_DISPLAY=${LINE#*=};;
+            esac
+        fi
+    done < $FILE
+
+    if [[ $HIDDEN == "true" || $NO_DISPLAY == "true" ]];
+    then
+        continue
+    fi
+
+    if [[ $EXEC == *\"* ]];
+    then
+        EXEC=$(echo $EXEC | sed -e 's/"/\\"/g')
+    fi
+
+    if [[ ! -z $OUTPUT ]];
+    then
+        OUTPUT+=', '
+    fi
+
+    printf -v OBJ '{"name": "%s", "icon": "%s", "exec": "%s"}' "$NAME" "$ICON" "$EXEC"
+    OUTPUT+=$OBJ
+done
+
+eww update apps="[$OUTPUT]"
