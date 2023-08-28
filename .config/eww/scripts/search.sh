73705ffff4d4d4e380d33dd2270f9a5850063888
Nicholas Bowers <greennick7777@gmail.com>
2023-08-28 01:55:56 -0400

    Refactor search script and add icon lookup

--- .config/eww/scripts/search.sh
+++ .config/eww/scripts/search.sh
@@ -1,71 +1,90 @@
-#!/bin/bash
-IFS=':' read -ra DIRS <<< $XDG_DATA_DIRS
-DIRS+=($XDG_DATA_HOME)
-FILES=()
-for DIR in "${DIRS[@]}";
-do
-    if [[ $DIR != */ ]];
+#!/bin/sh
+xdg_subdirs() {
+  echo "$XDG_DATA_HOME:$XDG_DATA_DIRS" \
+    | awk -v dir="$1" '{
+      split($0, arr, ":");
+      for (i in arr)
+        print arr[i] ~ /\w+\/$/ ? arr[i] dir : arr[i] "/" dir
+      }'
+}
+
+find_icon_path () {
+  THEME=$1
+  if [ -f $2 ]; then
+    ICON_PATH=$2
+  fi
+
+  if [ -d "$HOME/.icons/$THEME" ]; then
+    ICON_PATH=$(find "$HOME/.icons/$THEME/scalable" -name "$2.svg" \
+      || find "$HOME/.icons/$THEME/32" -name "$2.svg" -or -name "$2.png")
+  fi
+
+  if [ -z "$ICON_PATH" ]; then
+    ICON_PATH=$(xdg_subdirs "icons/$THEME" \
+      | while read -r DIR; do [ -d "$DIR" ] && echo "$DIR"; done \
+      | while read -r DIR; do
+        echo $(find "$DIR/scalable" -name "$2.svg" \
+          || find "$DIR/32" -name "$2.svg" -or -name "$2.png")
+      done)
+  fi
+
+  if [ -z "$ICON_PATH" ] && [ $THEME != "hicolor" ]; then
+    find_icon_path hicolor $2
+  elif [ -z "$ICON_PATH" ]; then
+    echo $2
+  else
+    echo $ICON_PATH | awk '{ print $1 }'
+  fi
+}
+
+xdg_desktop_parse() {
+  PARSE=false
+  HIDDEN=false
+  NO_DISPLAY=false
+  while read -r LINE;
+  do
+    if [ "$LINE" = "[Desktop Entry]" ];
     then
-        DIR="${DIR}/applications"
-    else
-        DIR="${DIR}applications"
+      PARSE=true
+      continue
     fi
 
-    if [[ -d $DIR ]]; then
-        FILES+=($(find $DIR/*.desktop))
-    fi
-done
-
-TEST=0
-for FILE in "${FILES[@]}";
-do
-    PARSE=false
-    HIDDEN=false
-    NO_DISPLAY=false
-    while IFS='' read -r LINE;
-    do
-        if [[ $LINE == "[Desktop Entry]" ]];
-        then
-            PARSE=true
-            continue
-        fi
-
-        if [[ $PARSE == "true" ]];
-        then
-            case $LINE in
-                \[*\])
-                    break;;
-                Name=*)
-                    NAME=${LINE#*=};;
-                Icon=*)
-                    ICON=${LINE#*=};;
-                Exec=*)
-                    EXEC=${LINE#*=};;
-                Hidden=*)
-                    HIDDEN=${LINE#*=};;
-                NoDisplay=*)
-                    NO_DISPLAY=${LINE#*=};;
-            esac
-        fi
-    done < $FILE
-
-    if [[ $HIDDEN == "true" || $NO_DISPLAY == "true" ]];
+    if [ $PARSE = "true" ];
     then
-        continue
+      case $LINE in
+        \[*\])
+          break;;
+        Name=*)
+          NAME=${LINE#*=};;
+        Icon=*)
+          ICON=${LINE#*=};;
+        Exec=*)
+          EXEC=${LINE#*=};;
+        Hidden=*)
+          HIDDEN=${LINE#*=};;
+        NoDisplay=*)
+          NO_DISPLAY=${LINE#*=};;
+      esac
     fi
+  done < "$1"
 
-    if [[ $EXEC == *\"* ]];
-    then
-        EXEC=$(echo $EXEC | sed -e 's/"/\\"/g')
-    fi
+  if [ "$HIDDEN" = "true" ] || [ "$NO_DISPLAY" = "true" ];
+  then
+    return
+  fi
 
-    if [[ ! -z $OUTPUT ]];
-    then
-        OUTPUT+=', '
-    fi
+  ICON=$(find_icon_path "$2" "$ICON")
+  EXEC=$(echo "$EXEC" | sed -e 's/"/\\"/g' -e 's/ %\w//g')
 
-    printf -v OBJ '{"name": "%s", "icon": "%s", "exec": "%s"}' "$NAME" "$ICON" "$EXEC"
-    OUTPUT+=$OBJ
-done
+  printf '{"name":"%s", "icon": "%s", "exec": "%s"}\n' \
+    "$NAME" "$ICON" "$EXEC"
+}
 
-eww update apps="[$OUTPUT]"
+ICON_THEME=$1
+APPS=$(xdg_subdirs "applications" \
+  | while read -r DIR; do [ -d "$DIR" ] && echo "$DIR"; done \
+  | xargs -I{} find {} -name "*.desktop" \
+  | while read -r FILE; do xdg_desktop_parse "$FILE" "$ICON_THEME"; done \
+  | awk '{ s = (NR == 1 ? $0 : s", "$0) } END { print "["s"]" }')
+
+eww update apps="$APPS"
